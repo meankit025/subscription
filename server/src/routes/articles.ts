f46ccdeb59acbf6664284f16c20c67f88204e401
meankit025 <70406910+meankit025@users.noreply.github.com>
2022-03-22 12:09:09 +0530

    first commit

---
+++ server/src/routes/articles.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import User from "../models/user";
+import Article from "../models/article";
+import { checkAuth } from "../middleware/checkAuth";
+import { stripe } from "../utils/stripe";
+
+const router = express.Router();
+
+router.get("/", checkAuth, async (req, res) => {
+  const user = await User.findOne({ email: req.user });
+  console.log(user.stripeCustomerId);
+
+  const subscriptions = await stripe.subscriptions.list(
+    {
+      customer: user.stripeCustomerId,
+      status: "all",
+      expand: ["data.default_payment_method"],
+    },
+    {
+      apiKey: process.env.STRIPE_SECRET_KEY,
+    }
+  );
+
+  if (!subscriptions.data.length) return res.json([]);
+
+  //@ts-ignore
+  const plan = subscriptions.data[0].plan.nickname;
+
+  if (plan === "Diamond") {
+    const articles = await Article.find({ access: "Diamond" });
+    return res.json(articles);
+  } else if (plan === "Gold") {
+    const articles = await Article.find({
+      access: { $in: ["Diamond", "Gold"] },
+    });
+    return res.json(articles);
+  } else {
+    const articles = await Article.find({});
+    return res.json(articles);
+  }
+
+  res.json(plan);
+});
+
+export default router;
